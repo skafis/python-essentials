85e879f843c93e6414ffced214c170cefb3d66ef
skafis <franciskipchumba5@gmail.com>
2016-02-03 15:35:19 +0300

    syntax

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def main():
+    a, b = 0,1
+    if a < b:
+        print ("a isless than b")
+        elif a > b:
+            print ("a is greater than b")
+        else:
+            print ("a is equal to b")
+if __name__=="__main__":main()
--- syntax.py
+++ syntax.py
@@ -3,5 +3,9 @@
 def main():
     a = {1,2,2,7,8,3,8}
     print (a)
+def func():
+    for i in range (10):
+        print (i, end='')
+    print ()
 
 if __name__=="__main__":main()
