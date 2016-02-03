a1e95e4a952f3c4780fb92295f0e4a78ca000965
skafis <franciskipchumba5@gmail.com>
2016-02-03 20:56:07 +0300

    elif

--- ifelse.py
+++ ifelse.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def main():
-
-    a,b = 3,2
-    if a<b
-        print('this is true:')
+    v='one'
+    if v=='one':
+        print ('v is one')
+    else v== 'two':
+        print ('v is two')
     else:
-        print('not true')
-
+        print('fgdfv')
 if __name__=="__main__":main()
