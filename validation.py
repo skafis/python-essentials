626fe671fe22d8b1ee2da3b365fff2f94ecf08fa
skafis <franciskipchumba5@gmail.com>
2016-02-04 23:43:50 +0300

    capturing data from user is okay

--- validation.py
+++ validation.py
@@ -4,8 +4,9 @@ import random
 def main():
     user_input = input("enter text or paragraph:")
     user_input = user_input.lower();
-    if type(user_input)== str:
-        array = list(user_input)
-        print random.choice(array)
+    print (user_input)
+    #if type(user_input)== str:
+     #   array = list(user_input)
+        #print random.choice(array)
 
 if __name__=="__main__":main()
