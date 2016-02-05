1fde9f197fcf068c8c4b16577dc68d10c7079d33
skafis <franciskipchumba5@gmail.com>
2016-02-05 22:30:35 +0300

    check tosee if word can assign

---
+++ Stringchunks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#a function that takes a string and a positive integer n, splits the string into parts of n length and returns them in an array. It is ok for the last element to have less characters than n.
+#If n is not a valid size(> 0) (or is absent), you should return an empty array.
+#If n is greater than the lenght of the string, you should return an array with the only element being the same string.
+
+def main():
+    a, b = input("enter a word and numnber:")
+    print (word,num)
+
+if __name__=="__name__": main()
