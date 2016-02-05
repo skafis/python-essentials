a3d82955ba1f8f5199809ab53a4e907f2c318995
skafis <franciskipchumba5@gmail.com>
2016-02-05 20:58:22 +0300

    counting arrays

---
+++ toyp1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def main():
+    num =0
+    array = [12,8,11,9,4,6,3,7,17]
+    for i in array:
+        if i<10:
+            num=num+1
+            print(num)
+if __name__=="__main__":main()
