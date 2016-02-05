507c62787c2ca3b8ec4e6c837c17de4b04fd9364
skafis <franciskipchumba5@gmail.com>
2016-02-05 20:29:14 +0300

    validation2

--- loop.py
+++ loop.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def main():
     s = "this is a string"
-    for c in s:
-        if c == 's': continue
-        print(c, end= '')
+    i=0
+    while(i < len(s)):
+        print(s[i], end='')
+        i += 1
+    else:
+        print('else')
 if __name__=="__name__": main()
