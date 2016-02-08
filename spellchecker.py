24a55fed3aff96f700ca88fcc5d06bae44a8b1e5
skafis <franciskipchumba5@gmail.com>
2016-02-08 09:51:48 +0300

    spliting the mispelt word

--- spellchecker.py
+++ spellchecker.py
@@ -9,3 +9,11 @@ def train(features):
     for f in features:
         model[f] += 1
     return model
+
+NWORDS = train(words(file('big.text').read()))
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def edist1(word):
+    splits = [(word[:i],word[i:]) for i in range (len(words)+1)]
+
