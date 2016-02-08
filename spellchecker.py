66ec2b689b57a4c9f257c71f42297ea62be1cd36
skafis <franciskipchumba5@gmail.com>
2016-02-08 10:11:43 +0300

    final

--- spellchecker.py
+++ spellchecker.py
@@ -22,5 +22,12 @@ def edist1(word):
     inserts = [a +c +b for a,b in splits for c in alphabet]
     return set (deletes + transposes + replaces + inserts)
 
+def known_edits2(word):
+    return set (e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
 
+def known(words): return set(w for w in words if w in NWORDS)
+
+def correct(word):
+    candidates = known ([word]) or known(edits1(word)) or known_edits2(word) or [word]
+    return max(candidates, key = NWORDS.get)
 
