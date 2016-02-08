c40ef9ddb27782027f2453f42cdc7186248b790e
skafis <franciskipchumba5@gmail.com>
2016-02-08 10:02:51 +0300

    returns all the modes

--- spellchecker.py
+++ spellchecker.py
@@ -20,6 +20,7 @@ def edist1(word):
     transposes = [a +b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
     replaces = [a + c +b[1:] for a, b in splits for c in alpahabet if b]
     inserts = [a +c +b for a,b in splits for c in alphabet]
+    return set (deletes + transposes + replaces + inserts)
 
 
 
