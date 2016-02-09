c83a86f7fcd69d11be16ce73d39f01cdeac4d946
skafis <franciskipchumba5@gmail.com>
2016-02-09 14:30:56 +0300

    search a text file

--- search.py
+++ search.py
@@ -1,7 +1,13 @@
 import re
-text = input("enter text or paragraph to search\n")
-find = input("enter the word you want to search\n")
-if re.search(r"[find]",text, re.M):
-    print (find + " has been found")
-else:
-    print("sorry "+find+ " cant be found try another word")
+with open('text.txt')as text:
+#    text = f.readlines()
+    #text = input("enter text or paragraph to search\n")
+    find = input("enter the word you want to search\n")
+    for line in text:
+        if find in line:
+            print (line)
+#        print (text)
+#        if re.search(r"[find]",text, re.M):
+#            print (find + " has been found")
+#        else:
+#            print("sorry "+find+ " cant be found try another word")
